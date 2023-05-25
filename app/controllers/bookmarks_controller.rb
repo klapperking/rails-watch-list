003d8d286c3b6030b613fc8c3ac7ab3bf0538171
Martin Klapper <martinklapper@tutanota.com>
2023-05-25 17:29:21 +0200

    bookmark CD actions - list U action

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,32 @@
 class BookmarksController < ApplicationController
+
+  def new
+    @bookmark = Bookmark.new
+    @list = List.find(params[:list_id])
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+
+    if @bookmark.save
+      redirect_to(list_path(@list))
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    bookmark = Bookmark.find(params[:id])
+    bookmark.destroy
+
+    redirect_to(list_path(params[:list_id]))
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(%i[movie_id comment])
+  end
 end
