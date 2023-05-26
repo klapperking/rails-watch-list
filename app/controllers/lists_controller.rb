99f7cfcdbdc82b5eaf332f4c4e480cb949ba1562
Martin Klapper <martinklapper@tutanota.com>
2023-05-26 11:27:01 +0200

    bookmark form moved list show

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,6 @@
 class ListsController < ApplicationController
   before_action :set_task, only: %i[show destroy]
+  before_action :set_bookmark, only: %i[show]
 
   def index
     @lists = List.all
@@ -22,13 +23,20 @@ class ListsController < ApplicationController
     redirect_to(lists_path, status: :see_other)
   end
 
+  private
+
+  def list_params
+    params.require(:list).permit(%i[name])
+  end
+
+  def set_bookmark
+    @bookmark = Bookmark.new
+  end
+
   protected
 
   def set_task
     @list = List.find(params[:id])
   end
 
-  def list_params
-    params.require(:list).permit(%i[name])
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :lists, except: %i[edit update] do
-    resources :bookmarks, only: %i[new create destroy]
+    resources :bookmarks, only: %i[create destroy]
   end
 end
