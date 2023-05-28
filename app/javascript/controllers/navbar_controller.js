167b0a44f3211785c46e5b6db915e26f3338d7a0
Martin Klapper <martinklapper@tutanota.com>
2023-05-28 18:33:22 +0200

    navbar styling for index and non-index

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -8,21 +8,23 @@ export default class extends Controller {
     if ((current.endsWith("lists")) || (current.endsWith("lists#"))) {
       this.element.classList.remove('sticky-top')
       this.element.classList.add('fixed-top')
-      this.element.classList.add('col-white')
     } else {
       console.log("Stick-page")
-      this.element.classList.remove('col-white')
       this.element.classList.remove('fixed-top')
-      this.element.style["background-color"] = "white"
       this.element.classList.add('sticky-top', 'top-0')
+      this.element.classList.add('navbar-lewagon-white')
     }
   }
 
   updateNavbar() {
-    if (window.scrollY >= window.innerHeight) {
+    if (window.scrollY >= (window.innerHeight / 2)) {
       this.element.classList.add("navbar-lewagon-white")
+
     } else {
-      this.element.classList.remove("navbar-lewagon-white")
+      if (!(current.endsWith('lists') || current.endsWith('lists#'))) {
+        this.element.classList.remove("navbar-lewagon-white")
+      }
     }
   }
+
 }
